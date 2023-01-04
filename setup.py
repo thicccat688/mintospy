13a652e3956002053f6b2a8e0b3601487c1a5cae
thicccat688 <tomasperestrelo21@gmail.com>
2023-01-04 11:56:39 +0000

    Bump up version to 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mintospy',
-    version='0.0.3',
+    version='0.0.4',
     license='MIT',
     author='Tomás Perestrelo',
     author_email='tomasperestrelo21@gmail.com',
